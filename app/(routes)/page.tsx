f0d6eb33122c53dec94f7c9be6005cc8b31aac92
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-12-19 21:59:05 +0600

    first commit

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <main>
+      <h1 className='text-center text-4xl font-bold py-3 tracking-wider text-green-500'>
+        Next.js + Node mailer
+      </h1>
+    </main>
+  );
+}
---
+++ app/api/email/route.ts
@@ -0,0 +1,56 @@
+import { NextResponse, NextRequest } from 'next/server';
+
+import nodemailer from 'nodemailer';
+
+export async function GET(req: Request) {
+  try {
+    const to = 'mdasikuzzaman.en@gmail.com';
+    const subject = 'Next js + Node mailer';
+    const message = 'Hi, from node mailer...';
+
+    const generateEmailHtml = (message: string, subject: string) => `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Template</title>
+  </head>
+  <body style="background: purple; padding: 10px; border-radius: 5px;">
+    <h1 style="text-align: center; color: white; font-size: 18px;">${subject}</h1>
+    <h1 style="text-align: center; color: white; font-size: 22px; margin-top: 10px;">${message}</h1>
+  </body>
+  </html>
+`;
+
+    const html = generateEmailHtml(message, subject);
+
+    // Create a nodemailer transporter
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'ashikmia557@gmail.com',
+        pass: process.env.APP_PASSWORD,
+      },
+    });
+
+    // Define the email options
+    const mailOptions = {
+      from: 'ashikmia557@gmail.com',
+      to,
+      subject,
+      html,
+    };
+
+    // Send the email
+    try {
+      await transporter.sendMail(mailOptions);
+      return NextResponse.json({ message: 'Email sent successfully!!!' });
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({ message: 'Email not sent.' });
+    }
+  } catch (error) {
+    return NextResponse.json({ Error: 'Something went wrong' });
+  }
+}
